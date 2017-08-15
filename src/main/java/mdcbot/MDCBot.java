f33821c61bf77360799cb1aaae09cb04aa20d338
bright_spark <markpassey@btinternet.com>
2017-08-15 01:06:23 +0100

    Setup log4j

--- src/main/java/mdcbot/MDCBot.java
+++ src/main/java/mdcbot/MDCBot.java
@@ -5,51 +5,57 @@ import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.exceptions.RateLimitedException;
-import net.dv8tion.jda.core.hooks.EventListener;
 import org.apache.log4j.Logger;
 
 import javax.security.auth.login.LoginException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.File;
+import java.net.MalformedURLException;
 
 public class MDCBot
 {
     //JDA: https://github.com/DV8FromTheWorld/JDA
+    //Example: https://github.com/DV8FromTheWorld/JDA/blob/master/src/examples/java/MessageListenerExample.java
 
     public static final String NAME = "MDCBot";
+    public static final String CLIENT_ID = "346744066987261962";
     public static final String TOKEN = "MzQ2NzQ0MDY2OTg3MjYxOTYy.DHOTrg.LlAqQw3ZdC8LT4fNhREcp-Ae0gM";
-    public static final String AUTH_URL = "https://discordapp.com/oauth2/authorize?&client_id=346744066987261962&scope=bot";
+    public static final String AUTH_URL = "https://discordapp.com/oauth2/authorize?&client_id=" + CLIENT_ID + "&scope=bot";
 
-    public static final Logger LOG = Logger.getLogger(NAME);
+    public static final File RESOURCES_DIR = new File("src/main/resources");
 
     public static JDA jda;
 
-    private static List<EventListener> listeners = new ArrayList<>();
+    public static Logger LOG = Logger.getLogger(NAME);
+
+    static
+    {
+        //Set log4j configuration file
+        try
+        {
+            System.setProperty("log4j.configuration", new File(RESOURCES_DIR, "log4j.properties").toURI().toURL().toString());
+        }
+        catch(MalformedURLException e)
+        {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String... args)
     {
-        init();
+        LOG.info("Initialising bot...");
 
         try
         {
             jda = new JDABuilder(AccountType.BOT)
                     .setToken(TOKEN)
-                    .addEventListener(listeners)
+                    .addEventListener(new MessageReceivedListener())
                     .buildBlocking();
         }
         catch(LoginException | InterruptedException | RateLimitedException e)
         {
             e.printStackTrace();
         }
-    }
 
-    public static void init()
-    {
-        regListener(new MessageReceivedListener());
-    }
-
-    private static void regListener(EventListener listener)
-    {
-        listeners.add(listener);
+        LOG.info("Initialisation complete");
     }
 }
