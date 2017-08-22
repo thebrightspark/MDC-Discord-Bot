c35faa8c6764d3af4ae6f3d7c9cf8f7dc90a521b
alexcouch <alcouch65@gmail.com>
2017-08-22 15:16:33 -0700

    Cleaned up the UserPoints math by like a shit ton. Also, the bot now private messages the user whose points have been changed, upon change.

--- src/main/java/mdcbot/command/CommandManagePoints.java
+++ src/main/java/mdcbot/command/CommandManagePoints.java
@@ -3,6 +3,7 @@ package mdcbot.command;
 import com.jagrosh.jdautilities.commandclient.CommandEvent;
 import mdcbot.MDCBot;
 import mdcbot.Util;
+import mdcbot.points.UserPoints;
 import net.dv8tion.jda.core.entities.User;
 
 public class CommandManagePoints extends CommandBase{
@@ -16,16 +17,18 @@ public class CommandManagePoints extends CommandBase{
             if(args.length == 3){
                 if(user.getId().equals(args[1])) {
                     int points = Integer.parseInt(args[2]);
-                    MDCBot.points.addOrSubPoints(user, points, !args[0].equalsIgnoreCase("add") && args[0].equalsIgnoreCase("remove"));
-                    int r = MDCBot.points.remainders;
-                    event.reply(MDCBot.jda.getUserById(Long.parseLong(args[1])) + " had " + r + " points " +
+                    UserPoints.addOrSubPoints(user, points, !args[0].equalsIgnoreCase("add") && args[0].equalsIgnoreCase("remove"));
+                    event.reply(MDCBot.jda.getUserById(Long.parseLong(args[1])) + " had " + points + " points " +
                             (args[0].equalsIgnoreCase("add") ? "added" : (args[0].equalsIgnoreCase("remove") ? "removed" : null))
                     );
+                    user.openPrivateChannel().queue((channel)->{
+                        channel.sendMessage("Aye nigga, ya'll now have " + UserPoints.getUsersPoints(user) + " points. If ya'll tink dis is a prob, talk to yo admin, aight?").queue();
+                    });
                 }
             }else if(args.length == 2){
                 if(args[0].equalsIgnoreCase("get")) {
                     if (user.getId().equals(args[1])) {
-                        event.reply(user.getName() + " has " + MDCBot.points.getUsersPoints(user) + " points.");
+                        event.reply(user.getName() + " has " + UserPoints.getUsersPoints(user) + " points.");
                     }
                 }
             }else{
--- src/main/java/mdcbot/points/UserPoints.java
+++ src/main/java/mdcbot/points/UserPoints.java
@@ -8,28 +8,19 @@ import java.util.Map;
 public class UserPoints {
 
     private static Map<User, Integer> userPoints = new HashMap<>();
-    public int remainders;
 
-    public void addOrSubPoints(User user, int points, boolean deduct){
+    public static void addOrSubPoints(User user, int points, boolean deduct){
         int priorPoints = userPoints.getOrDefault(user, 0);
         if(userPoints.containsKey(user)) {
-            if (userPoints.get(user) >= 0) {
-                int newPoints = !deduct ? priorPoints + points : (deduct ? (priorPoints - points) : 0);
-                if (deduct && newPoints <= 0) {
-                    newPoints = 0;
-                }
-                if(deduct && newPoints == 0) remainders = userPoints.get(user);
-                else remainders = points;
-                userPoints.remove(user);
-                userPoints.put(user, newPoints);
-                if(!deduct) remainders = points;
-            }
+            int newPoints = !deduct ? priorPoints + points : (deduct ? (priorPoints - points) : 0);
+            userPoints.remove(user);
+            userPoints.put(user, newPoints);
         }else{
             userPoints.put(user, points);
         }
     }
 
-    public int getUsersPoints(User user){
+    public static int getUsersPoints(User user){
         return userPoints.get(user);
     }
 }
